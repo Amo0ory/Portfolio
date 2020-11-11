8b712bc93ff5fbadbde0b37afaf3932e253b1bd6
Your NameAmer <shbh2010@hotmail.com>
2020-11-12 12:27:56 +1300

    Initial Comitt

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './sass/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      
+     
+      <div className="home-page">Hello Amer</div>
+    </div>
+
+   
+  );
+}
+
+export default App;
