3728c343b6f26bfe8a30351bcf58526ac1e8d7f4
Your NameAmer <shbh2010@hotmail.com>
2020-11-30 23:34:46 +1300

    clean

--- portfolio-site/src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/style.scss';
-import AppRouter from './routers/AppRouter';
- 
-
-ReactDOM.render(<AppRouter />,document.getElementById('app'));
-
